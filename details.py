bd7c76b92073c36c379bd51ae7af7c96a90fbcdc
Shubham Kumar <130787773+student12as@users.noreply.github.com>
2023-05-31 15:06:05 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
-api_id = 26435700
-api_hash = "527cf5174e120a9093611bc69d7b7709"
-bot_token = "6180767554:AAGKw-Eo6L0DXlL4BsrDmKItUewgdKGAtGo"
+api_id = 1463957
+api_hash = "17bbc6e28fb5f22d4cc0220e0175a700"
+bot_token = "6132860601:AAEb4HJOod6_3OYNks07mmjNZSbDcG9hwcU"
 
